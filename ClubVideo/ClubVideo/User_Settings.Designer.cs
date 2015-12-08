b451e98bc8e7d100a762df8d09694ced2dd24803
antoine3990 <z.skyline@hotmail.com>
2015-12-07 23:08:02 -0500

    ...

--- ClubVideo/ClubVideo/User_Settings.Designer.cs
+++ ClubVideo/ClubVideo/User_Settings.Designer.cs
@@ -142,7 +142,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(600, 420);
+            this.ClientSize = new System.Drawing.Size(600, 199);
             this.Controls.Add(this.B_fonts);
             this.Controls.Add(this.B_password);
             this.Controls.Add(this.B_lang);
