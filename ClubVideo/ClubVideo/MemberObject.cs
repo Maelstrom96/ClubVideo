876b5f4c34a00937c57eac9f88b23f00a93c3ba5
Maelstrom96 <alexandrovich8@live.ca>
2015-12-15 12:22:23 -0500

    Ajout d'un objet membre

---
+++ ClubVideo/ClubVideo/MemberObject.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClubVideo
+{
+    class MemberObject
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string Adress { get; set; }
+        public string PostalCode { get; set; }
+        public string City { get; set; }
+        public string Province { get; set; }
+        public int TelNumber { get; set; }
+    }
+}
