9a4486ca4a4dff5c58f1df42d7a5167ec777b6ed
mlussier17 <math.lussier@hotmail.com>
2015-12-15 14:04:53 -0500

    Modif members

--- ClubVideo/ClubVideo/UpdateMembers.cs
+++ ClubVideo/ClubVideo/UpdateMembers.cs
@@ -18,17 +18,23 @@ namespace ClubVideo
             InitializeComponent();
         }
 
-        public UpdateMembers(BindingSource source, string header)
+        public UpdateMembers(BindingSource source, string header, bool modif)
         {
             InitializeComponent();
             Text = header;
             _bs = source;
-
-            btn_Next.Visible = true;
-            btn_Previous.Visible = true;
-            btn_Add.Visible = false;
-
-            foreach (var tb in this.Controls.OfType<TextBox>()) tb.ReadOnly = true;
+            if (!modif)
+            {
+                btn_Next.Visible = true;
+                btn_Previous.Visible = true;
+                btn_Add.Visible = false;
+                foreach (var tb in this.Controls.OfType<TextBox>()) tb.ReadOnly = true;
+            }
+            if (modif)
+            {
+                btn_Modify.Visible = true;
+                btn_Add.Visible = false;
+            }
             txb_Name.DataBindings.Add("text", _bs, "NAME");
             txb_LastName.DataBindings.Add("text", _bs, "LAST_NAME");
             txb_Adress.DataBindings.Add("text", _bs, "ADDRESS");
@@ -71,6 +77,26 @@ namespace ClubVideo
         private void btn_Cancel_Click(object sender, EventArgs e)
         {
             this.Close();
+        }
+
+        private void btn_Modify_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                MemberObject oMember = new MemberObject();
+                oMember.Name = txb_Name.Text.ToString();
+                oMember.LastName = txb_LastName.Text.ToString();
+                oMember.Address = txb_Adress.Text.ToString();
+                oMember.PostalCode = txb_PostalCode.Text.ToString();
+                oMember.City = txb_City.Text.ToString();
+                oMember.Province = txb_Province.Text.ToString();
+                oMember.TelNumber = int.Parse(txb_Telephone.Text.ToString());
+                Database_Connector.Insert.Member(oMember);
+                MessageBox.Show("Modification fait!");
+                this.Close();
+
+            }
+            catch (Exception ex) { MessageBox.Show(ex.Message); }
         }
     }
 }
