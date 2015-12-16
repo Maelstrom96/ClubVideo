9adea19a0a2168b60a0b990e945c4e4f8b71ff25
Maelstrom96 <alexandrovich8@live.ca>
2015-12-15 21:35:28 -0500

    Ajout du support multilangue et du mode modification pour le form

--- ClubVideo/ClubVideo/Manage_Movies.cs
+++ ClubVideo/ClubVideo/Manage_Movies.cs
@@ -14,18 +14,48 @@ namespace ClubVideo
     {
         BindingSource source;
         DataTable ds;
+        private bool Modification_Mode;
         
 
-        public Manage_Movies()
+        public Manage_Movies(bool Modification = false)
         {
             InitializeComponent();
             Database.Update.Movies();
+
+            Modification_Mode = Modification;
+
+            LoadMovies();
+            LoadLanguage();
+        }
+
+        private void LoadMovies()
+        {
             ds = Database.GetData.Movies();
             source = new BindingSource();
             source.DataSource = ds;
             dgv_SearchResults.DataSource = source;
             dgv_SearchResults.Columns[8].Visible = false;
-            
+        }
+
+        private void LoadLanguage()
+        {
+            // Header text
+            if (Modification_Mode) Text = Main.resManager.GetString("Manage_Movies_Header_Modif", Main.culInfo);
+            else Text = Main.resManager.GetString("Manage_Movies_Header", Main.culInfo);
+
+            // DGV Column text
+            foreach (DataGridViewColumn column in dgv_SearchResults.Columns)
+            {
+                column.HeaderText = Main.resManager.GetString("Manage_Movies_" + column.Name, Main.culInfo);
+            }
+
+            // lb_Search text
+            lb_Search.Text = Main.resManager.GetString("Manage_Movies_Search", Main.culInfo);
+
+            // Set tb_Search position relative to lb_Search
+            int height_Diff = (tb_Search.Height - lb_Search.Height) / 2;
+            SizeF width = lb_Search.CreateGraphics().MeasureString(lb_Search.Text, lb_Search.Font);
+            tb_Search.Location = new Point(lb_Search.Location.X + (int)width.Width, lb_Search.Location.Y - height_Diff);
         }
 
         private void Search()
