ac5fea32d55defe8ac543891042f0e4d44e3d03e
Maelstrom96 <alexandrovich8@live.ca>
2015-12-15 23:09:30 -0500

    Fix du layout

--- ClubVideo/ClubVideo/Admin_Menu.Designer.cs
+++ ClubVideo/ClubVideo/Admin_Menu.Designer.cs
@@ -31,11 +31,11 @@
             this.bt_ManageMovies = new System.Windows.Forms.Button();
             this.bt_Back = new System.Windows.Forms.Button();
             this.bt_ManageMembers = new System.Windows.Forms.Button();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.bt_Settings = new System.Windows.Forms.Button();
             this.bt_ManageUsers = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.SuspendLayout();
             // 
             // bt_ManageMovies
@@ -46,9 +46,10 @@
             this.bt_ManageMovies.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.bt_ManageMovies.Image = global::ClubVideo.Properties.Resources.ManageMovies;
             this.bt_ManageMovies.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
-            this.bt_ManageMovies.Location = new System.Drawing.Point(615, -1);
+            this.bt_ManageMovies.Location = new System.Drawing.Point(481, -1);
+            this.bt_ManageMovies.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.bt_ManageMovies.Name = "bt_ManageMovies";
-            this.bt_ManageMovies.Size = new System.Drawing.Size(134, 134);
+            this.bt_ManageMovies.Size = new System.Drawing.Size(118, 121);
             this.bt_ManageMovies.TabIndex = 1;
             this.bt_ManageMovies.Text = "manage Movies";
             this.bt_ManageMovies.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
@@ -62,9 +63,10 @@
             this.bt_Back.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.bt_Back.Image = global::ClubVideo.Properties.Resources.Back;
             this.bt_Back.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
-            this.bt_Back.Location = new System.Drawing.Point(1067, -1);
+            this.bt_Back.Location = new System.Drawing.Point(866, -1);
+            this.bt_Back.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.bt_Back.Name = "bt_Back";
-            this.bt_Back.Size = new System.Drawing.Size(134, 134);
+            this.bt_Back.Size = new System.Drawing.Size(118, 121);
             this.bt_Back.TabIndex = 2;
             this.bt_Back.Text = "back";
             this.bt_Back.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
@@ -79,23 +81,14 @@
             this.bt_ManageMembers.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.bt_ManageMembers.Image = global::ClubVideo.Properties.Resources.ManageMembers;
             this.bt_ManageMembers.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
-            this.bt_ManageMembers.Location = new System.Drawing.Point(755, -1);
+            this.bt_ManageMembers.Location = new System.Drawing.Point(603, -1);
+            this.bt_ManageMembers.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.bt_ManageMembers.Name = "bt_ManageMembers";
-            this.bt_ManageMembers.Size = new System.Drawing.Size(134, 134);
+            this.bt_ManageMembers.Size = new System.Drawing.Size(118, 121);
             this.bt_ManageMembers.TabIndex = 2;
             this.bt_ManageMembers.Text = "manage Members";
             this.bt_ManageMembers.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
             this.bt_ManageMembers.UseVisualStyleBackColor = true;
-            // 
-            // pictureBox1
-            // 
-            this.pictureBox1.Image = global::ClubVideo.Properties.Resources.Line1;
-            this.pictureBox1.Location = new System.Drawing.Point(1027, -1);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(43, 134);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox1.TabIndex = 3;
-            this.pictureBox1.TabStop = false;
             // 
             // bt_Settings
             // 
@@ -105,9 +98,10 @@
             this.bt_Settings.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.bt_Settings.Image = global::ClubVideo.Properties.Resources.Settings;
             this.bt_Settings.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
-            this.bt_Settings.Location = new System.Drawing.Point(895, -1);
+            this.bt_Settings.Location = new System.Drawing.Point(725, -1);
+            this.bt_Settings.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.bt_Settings.Name = "bt_Settings";
-            this.bt_Settings.Size = new System.Drawing.Size(134, 134);
+            this.bt_Settings.Size = new System.Drawing.Size(118, 121);
             this.bt_Settings.TabIndex = 4;
             this.bt_Settings.Text = "settings";
             this.bt_Settings.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
@@ -121,9 +115,10 @@
             this.bt_ManageUsers.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.bt_ManageUsers.Image = global::ClubVideo.Properties.Resources.ManageMovies;
             this.bt_ManageUsers.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
-            this.bt_ManageUsers.Location = new System.Drawing.Point(475, -1);
+            this.bt_ManageUsers.Location = new System.Drawing.Point(359, -1);
+            this.bt_ManageUsers.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.bt_ManageUsers.Name = "bt_ManageUsers";
-            this.bt_ManageUsers.Size = new System.Drawing.Size(134, 134);
+            this.bt_ManageUsers.Size = new System.Drawing.Size(118, 121);
             this.bt_ManageUsers.TabIndex = 1;
             this.bt_ManageUsers.Text = "manage Users";
             this.bt_ManageUsers.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
@@ -138,33 +133,46 @@
             this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button2.Image = global::ClubVideo.Properties.Resources.ManageMovies;
             this.button2.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
-            this.button2.Location = new System.Drawing.Point(335, -1);
+            this.button2.Location = new System.Drawing.Point(237, -1);
+            this.button2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(134, 134);
+            this.button2.Size = new System.Drawing.Size(118, 121);
             this.button2.TabIndex = 1;
             this.button2.Text = "manage Groups";
             this.button2.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
             this.button2.UseVisualStyleBackColor = true;
             // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::ClubVideo.Properties.Resources.Line1;
+            this.pictureBox2.Location = new System.Drawing.Point(835, -1);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(2);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(39, 121);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 5;
+            this.pictureBox2.TabStop = false;
+            // 
             // Admin_Menu
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1198, 132);
+            this.ClientSize = new System.Drawing.Size(983, 119);
             this.ControlBox = false;
+            this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.bt_Settings);
-            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.bt_ManageUsers);
             this.Controls.Add(this.bt_ManageMovies);
             this.Controls.Add(this.bt_Back);
             this.Controls.Add(this.bt_ManageMembers);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "Admin_Menu";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -175,9 +183,9 @@
         private System.Windows.Forms.Button bt_ManageMovies;
         private System.Windows.Forms.Button bt_ManageMembers;
         private System.Windows.Forms.Button bt_Back;
-        private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Button bt_Settings;
         private System.Windows.Forms.Button bt_ManageUsers;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pictureBox2;
     }
 }
