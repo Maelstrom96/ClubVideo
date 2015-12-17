a524bd6a9386dc23cc7cfc22af2e174d46f2a87f
Maelstrom96 <alexandrovich8@live.ca>
2015-12-17 12:05:16 -0500

    Ajout d'un nouveau form pour les catégories (Layout fait)

---
+++ ClubVideo/ClubVideo/CategoryCU.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace ClubVideo
+{
+    partial class CategoryCU
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.tb_Name_Fr = new System.Windows.Forms.TextBox();
+            this.bt_OK = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tb_Name_En = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.tb_Price = new System.Windows.Forms.TextBox();
+            this.tb_Description_En = new System.Windows.Forms.TextBox();
+            this.tb_Description_Fr = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(4, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(110, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "name_fr :";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // tb_Name_Fr
+            // 
+            this.tb_Name_Fr.Location = new System.Drawing.Point(120, 25);
+            this.tb_Name_Fr.Name = "tb_Name_Fr";
+            this.tb_Name_Fr.Size = new System.Drawing.Size(181, 20);
+            this.tb_Name_Fr.TabIndex = 1;
+            // 
+            // bt_OK
+            // 
+            this.bt_OK.Location = new System.Drawing.Point(239, 324);
+            this.bt_OK.Name = "bt_OK";
+            this.bt_OK.Size = new System.Drawing.Size(75, 23);
+            this.bt_OK.TabIndex = 2;
+            this.bt_OK.Text = "ok";
+            this.bt_OK.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(4, 54);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(110, 17);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "name_en :";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // tb_Name_En
+            // 
+            this.tb_Name_En.Location = new System.Drawing.Point(120, 51);
+            this.tb_Name_En.Name = "tb_Name_En";
+            this.tb_Name_En.Size = new System.Drawing.Size(181, 20);
+            this.tb_Name_En.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.Location = new System.Drawing.Point(4, 97);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(110, 17);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "decription_fr :";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // label4
+            // 
+            this.label4.Location = new System.Drawing.Point(4, 187);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(110, 17);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "decription_en :";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // label5
+            // 
+            this.label5.Location = new System.Drawing.Point(117, 286);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(110, 17);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "price :";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // tb_Price
+            // 
+            this.tb_Price.Location = new System.Drawing.Point(239, 283);
+            this.tb_Price.Name = "tb_Price";
+            this.tb_Price.Size = new System.Drawing.Size(62, 20);
+            this.tb_Price.TabIndex = 1;
+            // 
+            // tb_Description_En
+            // 
+            this.tb_Description_En.Location = new System.Drawing.Point(120, 184);
+            this.tb_Description_En.Multiline = true;
+            this.tb_Description_En.Name = "tb_Description_En";
+            this.tb_Description_En.Size = new System.Drawing.Size(181, 78);
+            this.tb_Description_En.TabIndex = 1;
+            // 
+            // tb_Description_Fr
+            // 
+            this.tb_Description_Fr.Location = new System.Drawing.Point(120, 94);
+            this.tb_Description_Fr.Multiline = true;
+            this.tb_Description_Fr.Name = "tb_Description_Fr";
+            this.tb_Description_Fr.Size = new System.Drawing.Size(181, 78);
+            this.tb_Description_Fr.TabIndex = 1;
+            // 
+            // CategoryCU
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(326, 352);
+            this.Controls.Add(this.bt_OK);
+            this.Controls.Add(this.tb_Price);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.tb_Description_Fr);
+            this.Controls.Add(this.tb_Description_En);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.tb_Name_En);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tb_Name_Fr);
+            this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "CategoryCU";
+            this.Text = "CategoryCU";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tb_Name_Fr;
+        private System.Windows.Forms.Button bt_OK;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tb_Name_En;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox tb_Price;
+        private System.Windows.Forms.TextBox tb_Description_En;
+        private System.Windows.Forms.TextBox tb_Description_Fr;
+    }
+}
