41858487fb1592a908220f3466c4b9d8db282768
Maelstrom96 <alexandrovich8@live.ca>
2015-12-17 18:04:10 -0500

    Ajout de fonction pour load les catégories

--- ClubVideo/ClubVideo/Add_Movie_Menu.cs
+++ ClubVideo/ClubVideo/Add_Movie_Menu.cs
@@ -16,6 +16,7 @@ namespace ClubVideo
         {
             InitializeComponent();
             LoadLanguage();
+            LoadCategories();
         }
 
         private void radioButton1_CheckedChanged(object sender, EventArgs e)
@@ -48,7 +49,19 @@ namespace ClubVideo
             rb_Manual.Text = Main.resManager.GetString("Add_Movie_Menu_From_Manual", Main.culInfo);
             lb_Url.Text = Main.resManager.GetString("Add_Movie_Menu_URL", Main.culInfo);
             bt_OK.Text = Main.resManager.GetString("Add_Movie_Menu_OK", Main.culInfo);
-            bt_Cancel.Text = Main.resManager.GetString("Add_Mobie_Menu_Cancel", Main.culInfo);
+            bt_Cancel.Text = Main.resManager.GetString("Add_Movie_Menu_Cancel", Main.culInfo);
+        }
+
+        private void LoadCategories()
+        {
+            List<string> CategoriesList = Database.GetData.List("Categories", Main.resManager.GetString("Add_Movie_Menu_Category_Column", Main.culInfo));
+
+            cb_Categories.Items.Clear();
+
+            foreach (string category in CategoriesList)
+            {
+                cb_Categories.Items.Add(category);
+            }
         }
 
         private void MoveItemsForm(bool Down = true)
@@ -62,6 +75,9 @@ namespace ClubVideo
             //lb_Url.Location = new Point(lb_Url.Location.X, lb_Url.Location.Y + Deplacement);
             bt_Cancel.Location = new Point(bt_Cancel.Location.X, bt_Cancel.Location.Y + Deplacement);
             bt_OK.Location = new Point(bt_OK.Location.X, bt_OK.Location.Y + Deplacement);
+            lb_Line.Location = new Point(lb_Line.Location.X, lb_Line.Location.Y + Deplacement);
+            lb_Category.Location = new Point(lb_Category.Location.X, lb_Category.Location.Y + Deplacement);
+            cb_Categories.Location = new Point(cb_Categories.Location.X, cb_Categories.Location.Y + Deplacement);
 
             Height += Deplacement;
         }
@@ -82,13 +98,26 @@ namespace ClubVideo
             else if (IndexIDEnd != -1) id = URL.Substring(IndexIDStart, IndexIDEnd - IndexIDStart);
             else throw new Exception("ID_FORMAT_INVALID");
 
-            Database_Connector.Insert.Movie(Database_Connector.GetFromIMDB(id));
+            MovieObject movie = Database_Connector.GetFromIMDB(id);
+
+            movie.Category = CategoryObject.GetCategoryID(GetSelectedCategory());
+
+            Database_Connector.Insert.Movie(movie);
         }
 
         private void bt_Cancel_Click(object sender, EventArgs e)
         {
             DialogResult = DialogResult.Cancel;
             Close();
+        }
+
+        private string GetSelectedCategory()
+        {
+            if (cb_Categories.Text.Equals(string.Empty)) throw new Exception("CATEGORY_EMPTY");
+            if (!Database.GetData.List("Categories", Main.resManager.GetString("Add_Movie_Menu_Category_Column", Main.culInfo)).Contains(cb_Categories.Text))
+                throw new Exception("CATEGORY_INVALID");
+
+            return cb_Categories.Text;
         }
 
         private void bt_OK_Click(object sender, EventArgs e)
