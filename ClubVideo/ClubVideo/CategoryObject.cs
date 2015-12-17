785d889fd579f93f03595873b154bc3ea1372c78
Maelstrom96 <alexandrovich8@live.ca>
2015-12-17 13:12:06 -0500

    Fix : Ajout de public pour la classe

--- ClubVideo/ClubVideo/CategoryObject.cs
+++ ClubVideo/ClubVideo/CategoryObject.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace ClubVideo
 {
-    class CategoryObject
+    public class CategoryObject
     {
         public int ID { get; set; }
         public string Name_En { get; set; }
