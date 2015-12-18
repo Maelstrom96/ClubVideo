f42483439df9290207cb9e782d35afeb46811fe3
antoine3990 <z.skyline@hotmail.com>
2015-12-18 02:32:47 -0500

    push

--- ClubVideo/ClubVideo/UpdateMembers.Designer.cs
+++ ClubVideo/ClubVideo/UpdateMembers.Designer.cs
@@ -52,7 +52,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 83);
+            this.label1.Location = new System.Drawing.Point(15, 52);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(29, 13);
             this.label1.TabIndex = 0;
@@ -61,7 +61,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 53);
+            this.label2.Location = new System.Drawing.Point(15, 21);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(43, 13);
             this.label2.TabIndex = 0;
@@ -70,7 +70,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(226, 53);
+            this.label3.Location = new System.Drawing.Point(15, 120);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(45, 13);
             this.label3.TabIndex = 0;
@@ -79,7 +79,7 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(226, 91);
+            this.label4.Location = new System.Drawing.Point(15, 154);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(64, 13);
             this.label4.TabIndex = 0;
@@ -88,7 +88,7 @@
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(228, 124);
+            this.label5.Location = new System.Drawing.Point(15, 189);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(26, 13);
             this.label5.TabIndex = 0;
@@ -97,7 +97,7 @@
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(228, 161);
+            this.label6.Location = new System.Drawing.Point(15, 228);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(49, 13);
             this.label6.TabIndex = 0;
@@ -106,7 +106,7 @@
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(12, 124);
+            this.label7.Location = new System.Drawing.Point(15, 86);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(58, 13);
             this.label7.TabIndex = 0;
@@ -114,21 +114,21 @@
             // 
             // txb_Name
             // 
-            this.txb_Name.Location = new System.Drawing.Point(75, 53);
+            this.txb_Name.Location = new System.Drawing.Point(94, 18);
             this.txb_Name.Name = "txb_Name";
             this.txb_Name.Size = new System.Drawing.Size(100, 20);
             this.txb_Name.TabIndex = 0;
             // 
             // txb_LastName
             // 
-            this.txb_LastName.Location = new System.Drawing.Point(75, 84);
+            this.txb_LastName.Location = new System.Drawing.Point(94, 49);
             this.txb_LastName.Name = "txb_LastName";
             this.txb_LastName.Size = new System.Drawing.Size(100, 20);
             this.txb_LastName.TabIndex = 1;
             // 
             // txb_Telephone
             // 
-            this.txb_Telephone.Location = new System.Drawing.Point(76, 124);
+            this.txb_Telephone.Location = new System.Drawing.Point(95, 83);
             this.txb_Telephone.MaxLength = 10;
             this.txb_Telephone.Name = "txb_Telephone";
             this.txb_Telephone.Size = new System.Drawing.Size(100, 20);
@@ -137,14 +137,14 @@
             // 
             // txb_Adress
             // 
-            this.txb_Adress.Location = new System.Drawing.Point(307, 53);
+            this.txb_Adress.Location = new System.Drawing.Point(94, 117);
             this.txb_Adress.Name = "txb_Adress";
             this.txb_Adress.Size = new System.Drawing.Size(100, 20);
             this.txb_Adress.TabIndex = 3;
             // 
             // txb_PostalCode
             // 
-            this.txb_PostalCode.Location = new System.Drawing.Point(307, 88);
+            this.txb_PostalCode.Location = new System.Drawing.Point(94, 151);
             this.txb_PostalCode.MaxLength = 6;
             this.txb_PostalCode.Name = "txb_PostalCode";
             this.txb_PostalCode.Size = new System.Drawing.Size(100, 20);
@@ -152,14 +152,14 @@
             // 
             // txb_City
             // 
-            this.txb_City.Location = new System.Drawing.Point(307, 124);
+            this.txb_City.Location = new System.Drawing.Point(94, 186);
             this.txb_City.Name = "txb_City";
             this.txb_City.Size = new System.Drawing.Size(100, 20);
             this.txb_City.TabIndex = 5;
             // 
             // btn_Add
             // 
-            this.btn_Add.Location = new System.Drawing.Point(439, 252);
+            this.btn_Add.Location = new System.Drawing.Point(112, 295);
             this.btn_Add.Name = "btn_Add";
             this.btn_Add.Size = new System.Drawing.Size(75, 23);
             this.btn_Add.TabIndex = 7;
@@ -169,7 +169,7 @@
             // 
             // btn_Cancel
             // 
-            this.btn_Cancel.Location = new System.Drawing.Point(244, 251);
+            this.btn_Cancel.Location = new System.Drawing.Point(67, 324);
             this.btn_Cancel.Name = "btn_Cancel";
             this.btn_Cancel.Size = new System.Drawing.Size(75, 23);
             this.btn_Cancel.TabIndex = 11;
@@ -179,9 +179,9 @@
             // 
             // btn_Previous
             // 
-            this.btn_Previous.Location = new System.Drawing.Point(15, 251);
+            this.btn_Previous.Location = new System.Drawing.Point(0, 266);
             this.btn_Previous.Name = "btn_Previous";
-            this.btn_Previous.Size = new System.Drawing.Size(75, 23);
+            this.btn_Previous.Size = new System.Drawing.Size(106, 23);
             this.btn_Previous.TabIndex = 10;
             this.btn_Previous.Text = "Precedent";
             this.btn_Previous.UseVisualStyleBackColor = true;
@@ -190,9 +190,9 @@
             // 
             // btn_Next
             // 
-            this.btn_Next.Location = new System.Drawing.Point(122, 251);
+            this.btn_Next.Location = new System.Drawing.Point(105, 266);
             this.btn_Next.Name = "btn_Next";
-            this.btn_Next.Size = new System.Drawing.Size(75, 23);
+            this.btn_Next.Size = new System.Drawing.Size(107, 23);
             this.btn_Next.TabIndex = 9;
             this.btn_Next.Text = "Suivant";
             this.btn_Next.UseVisualStyleBackColor = true;
@@ -201,7 +201,7 @@
             // 
             // btn_Modify
             // 
-            this.btn_Modify.Location = new System.Drawing.Point(342, 251);
+            this.btn_Modify.Location = new System.Drawing.Point(17, 295);
             this.btn_Modify.Name = "btn_Modify";
             this.btn_Modify.Size = new System.Drawing.Size(75, 23);
             this.btn_Modify.TabIndex = 8;
@@ -214,7 +214,7 @@
             // 
             this.cb_Provinces.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.cb_Provinces.FormattingEnabled = true;
-            this.cb_Provinces.Location = new System.Drawing.Point(307, 161);
+            this.cb_Provinces.Location = new System.Drawing.Point(94, 225);
             this.cb_Provinces.Name = "cb_Provinces";
             this.cb_Provinces.Size = new System.Drawing.Size(100, 21);
             this.cb_Provinces.TabIndex = 6;
@@ -223,7 +223,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(541, 287);
+            this.ClientSize = new System.Drawing.Size(212, 364);
             this.Controls.Add(this.cb_Provinces);
             this.Controls.Add(this.btn_Modify);
             this.Controls.Add(this.btn_Next);
